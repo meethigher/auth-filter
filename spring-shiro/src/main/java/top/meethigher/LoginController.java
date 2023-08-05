4fafcb1987828d21f92592fcbe2d79644da7e4d2
meethigher <meethigher@qq.com>
2023-08-05 13:35:27 +0800

    feat: shiro init

---
+++ spring-shiro/src/main/java/top/meethigher/LoginController.java
@@ -0,0 +1,16 @@
+package top.meethigher;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class LoginController {
+
+    @RequestMapping("/login.html")
+    public String loginTemplate() {
+
+        return "login";
+    }
+
+}
